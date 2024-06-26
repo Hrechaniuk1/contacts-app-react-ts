b682dec820ee62b8e7bd8f390d9a9b5a45554480
Alex Hrechaniuk <147260817+Hrechaniuk1@users.noreply.github.com>
2024-06-26 15:46:54 +0300

    Initial commit

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+import {Link } from "react-router-dom";
+
+import css from './Navigation.module.css'
+
+export default function Navigation() {
+
+    return (
+        <div className={css.container}>
+            <Link to='/'>Home</Link>
+            <Link to='/contacts'>Contacts</Link>
+        </div>
+    )
+}
